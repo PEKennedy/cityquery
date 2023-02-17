33d6dc171a88f7df8601b58f08010354c713c0f9
kisenge <76875002+kisenge@users.noreply.github.com>
2023-02-16 22:21:05 -0400

    functioning buttons

--- src/components/molecules/FileMenu.js
+++ src/components/molecules/FileMenu.js
@@ -1,21 +1,43 @@
-import React from 'react';
 import { VStack, Text,HStack } from 'native-base';
 import FileControl from '../atoms/FileControl';
+import React, { useState } from 'react';
 
 const style = {
   menuContainer: {
     height: 50,
     width: '100%',
     backgroundColor: '#0F0',
+    alignItems: 'center',
+
+
   },
   menuText: {
     fontSize: 20,
     fontWeight: 500,
     color: '#000',
+    alignItems: 'center',
+    
   },
 };
 
 const FileMenu = () => {
+
+  const [cityFiles,setVal]= useState({});
+
+
+
+  function addFile(file, fileName){
+    //this.setState({ cityFiles:[...this.state.cityFiles,JSON.parse(file)] })
+    let newCityFiles = cityFiles;
+    newCityFiles[fileName] = JSON.parse(file);
+    setVal(newCityFiles);
+  }
+
+  //clear all files from the canvas
+  function clearCityFiles(){
+    setVal([]);
+  }
+
 
 
   return (
@@ -26,8 +48,10 @@ const FileMenu = () => {
         File Menu
       </Text>
       CityJSON Upload List:
-      <FileControl upId={"cityUpload"} clearId={"cityClear"}  fileType={".json"}
-            clearText={"Clear CityJSON Files"}  />
+
+        <FileControl upId={"cityUpload"} clearId={"cityClear"} fileType={".json"}
+            clearText={"Clear CityJSON Files"} addFile={addFile} clearFiles={clearCityFiles}/>
+
 
 
       <HStack style={style.innerContainer}>
