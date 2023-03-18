9d3d2b84b044170eaa75fd8f97d0505ccc6fb67d
Ryan Delong <rdelong@unb.ca>
2023-03-18 18:26:20 -0300

    Removing the title on the user manual page.

--- src/components/pages/UserManualPage.js
+++ src/components/pages/UserManualPage.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { VStack } from 'native-base';
-import PageTitle from '../atoms/PageTitle';
-import { strings } from '../../constants/strings';
 import NavBar from '../organisms/NavBar';
 
 const style = {
@@ -12,19 +10,12 @@ const style = {
     alignItems: 'center',
     padding: 20,
   },
-  userManualTitle: {
-    fontSize: 48,
-    fontWeight: 500,
-    color: '#ffffff',
-    marginBottom: 20,
-  },
 };
 
 const UserManualPage = () => {
   return (
     <VStack style={style.pageContainer}>
       <NavBar selected="User Manual" />
-      <PageTitle titleStyle={style.userManualTitle} title={strings.userManual} />
     </VStack>
   );
 };
